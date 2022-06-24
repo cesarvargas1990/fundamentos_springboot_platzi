76eb7aa0728e6b67cbd58c2f204ee421bb928f2c
cesarvargas1990 <ceavarga@bancolombia.com.co>
2022-06-24 11:59:29 -0500

    se agrega logger

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,8 +4,11 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentosplatzi.springboot.fundamentos.configuration.MyBeanWithProperties;
+import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import com.fundamentosplatzi.springboot.fundamentos.repository.MyCustomRepository;
 import com.fundamentosplatzi.springboot.fundamentos.repository.MyCustomRepositoryWserviceFake;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,25 +17,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
+
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyCustomRepository myCustomRepository;
 	private MyCustomRepositoryWserviceFake myCustomRepositoryWserviceFake;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
 	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
 								  MyBean myBean,
 								  MyBeanWithDependency myBeanWithDependency,
 								  MyCustomRepository myCustomRepository,
 								  MyCustomRepositoryWserviceFake myCustomRepositoryWserviceFake,
-								  MyBeanWithProperties myBeanWithProperties) {
+								  MyBeanWithProperties myBeanWithProperties,
+								  UserPojo userPojo) {
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myCustomRepository = myCustomRepository;
 		this.myCustomRepositoryWserviceFake = myCustomRepositoryWserviceFake;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
@@ -40,14 +48,27 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+
 		myBeanWithDependency.printWithDependency();
 		componentDependency.saludar();
 		System.out.println(myCustomRepository.getData());
 		System.out.println(myCustomRepositoryWserviceFake.getDataFromService());
 		myBean.print();
-		System.out.println("ejemplo aplcando sobrecarga de metodo...");
+		System.out.println("ejemplo aplicando sobrecarga de metodo...");
 		System.out.println(myCustomRepositoryWserviceFake.getDataFromService(1));
 		System.out.println("llamado a bean con propiedades leyendo application.propertiess");
 		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail()+" - "+userPojo.getPassword());
+		System.out.println(userPojo.getAge());
+		LOGGER.error("esto es un error del aplicativo");
+		try {
+			int a = 10/0;
+			LOGGER.debug("valor de a es: "+a);
+		} catch (Exception e) {
+			LOGGER.error("ocurrio un error: "+e.getMessage());
+			LOGGER.error("info detallada: "+e.getStackTrace());
+			LOGGER.error(e.getStackTrace());
+		}
 	}
 }
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,9 +1,14 @@
 package com.fundamentosplatzi.springboot.fundamentos.bean;
 
+import com.fundamentosplatzi.springboot.fundamentos.FundamentosApplication;
 import com.fundamentosplatzi.springboot.fundamentos.repository.MyCustomRepository;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.slf4j.LoggerFactory;
 
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
 
+    private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
     private MyOperation myOperation;
     private MyCustomRepository myCustomRepository;
     public MyBeanWithDependencyImplement(MyOperation myOperation, MyCustomRepository myCustomRepository) {
@@ -14,8 +19,10 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("ingreso metodo printWithDependency");
         int number = 1;
         System.out.println( myOperation.sum(number));
+        LOGGER.debug("valor de la variable number"+number);
         System.out.println("este es un ejemplo de la data de un bean que tiene dependencia llamando a custom repository");
         System.out.println(myCustomRepository.getData());
         System.out.println("hola desde mi bean wih dependency");
